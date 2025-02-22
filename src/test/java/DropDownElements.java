64a003909c5428fa6f3ae7f008467f7c2a44086f
Sorinus <sorin.cucereavu@gmail.com>
2025-02-22 19:07:06 +0200

    add drop down cum sa le gasesti si sa faci actiuni asupra lor

---
+++ src/test/java/DropDownElements.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class DropDownElements {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("https://trytestingthis.netlify.app/");
+        WebElement dropDown=webDriver.findElement(By.xpath("//select[@id='option']"));
+        Select select=new Select(dropDown);
+        select.selectByIndex(1);
+        Thread.sleep(3000);
+        select.selectByValue("option 2");
+        Thread.sleep(3000);
+        select.selectByVisibleText("Option 3");
+        webDriver.quit();
+
+    }
+}
