fe2c55bfb404bc7a7611c5ae029dff1a8ebd6746
Sorinus <sorin.cucereavu@gmail.com>
2025-03-09 15:12:03 +0200

    fac cum era

--- src/test/java/StepDefinition/LoginSteps.java
+++ src/test/java/StepDefinition/LoginSteps.java
@@ -31,7 +31,6 @@ public class LoginSteps extends AbstractStepDef {
     @When("User click submit")
     public void user_click_submit() throws InterruptedException {
         click(loginPage.getSubmitButton(), 2);
-        
     }
 
     @Then("User is redirect to the home page")
