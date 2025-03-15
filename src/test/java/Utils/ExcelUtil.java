14837a5f18609c6246c297cbad10825a454e8dcf
Sorinus <sorin.cucereavu@gmail.com>
2025-03-15 13:27:12 +0200

    excel

--- src/test/java/Utils/ExcelUtil.java
+++ src/test/java/Utils/ExcelUtil.java
@@ -8,9 +8,17 @@ import java.io.IOException;
 public class ExcelUtil {
     public static void main(String[] args) throws IOException {
 
-            XSSFWorkbook wb = new XSSFWorkbook("Excel/e.xlsx");
-            XSSFSheet sheet = wb.getSheet("Sheet1");
-            System.out.println(sheet.getPhysicalNumberOfRows());
+        XSSFWorkbook wb = new XSSFWorkbook("Excel/e.xlsx");
+        XSSFSheet sheet = wb.getSheet("Sheet1");
+        System.out.println(sheet.getPhysicalNumberOfRows());
+        String nume = sheet.getRow(0).getCell(0).getStringCellValue();
+        String prenume = sheet.getRow(0).getCell(1).getStringCellValue();
+        double numeValue = sheet.getRow(1).getCell(1).getNumericCellValue();
+        String prenumeValue = sheet.getRow(1).getCell(0).getStringCellValue();
+        System.out.println(nume);
+        System.out.println(numeValue);
+        System.out.println(prenume);
+        System.out.println(prenumeValue);
 
     }
 }
