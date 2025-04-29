563a95b99387d8bdcee1d49e80d27ca4d4550df5
Sorin C <sorin.cucereavu@gmail.com>
2025-04-29 12:13:22 +0300

    put adaugare

---
+++ src/test/java/StepDefinition/Compare2PDFFiles.java
@@ -0,0 +1,20 @@
+package StepDefinition;
+
+import de.redsix.pdfcompare.PdfComparator;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.io.IOException;
+
+@Slf4j
+public class Compare2PDFFiles {
+    @Test
+    public void compare2PDFFiles() throws IOException {
+        String file1="PDFCompareFiles/actual.pdf";
+        String file2="PDFCompareFiles/expected.pdf";
+        String result="PDFCompareFiles/Result/result";
+        String ignoreFile="PDFCompareFiles/ignore.conf";
+        new PdfComparator(file1, file2).withIgnore(ignoreFile).compare().writeTo(result);
+        System.out.println("sucess");
+    }
+}
