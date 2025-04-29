699b8241b2659fa0be4c20012a422b1b232a3452
Sorin C <sorin.cucereavu@gmail.com>
2025-04-29 19:57:27 +0300

    adaug switch To cind schimbi intre taburi la chrome

---
+++ src/test/java/StepDefinition/Restul/SwitchWindowInBrowser.java
@@ -0,0 +1,30 @@
+package StepDefinition.Restul;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Set;
+
+public class SwitchWindowInBrowser {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.facebook.com/r.php?entry_point=login");
+        driver.findElement(By.xpath("//a[text()='Privacy Policy']")).click();
+        String currentWindow = driver.getWindowHandle();
+        Set<String> windows = driver.getWindowHandles();
+        for (String window : windows) {
+            if(!window.equals(currentWindow)){
+                driver.switchTo().window(window);
+            }
+        }
+        driver.findElement(By.xpath("//span[text()='Privacy Center']")).isDisplayed();
+        Thread.sleep(3000);
+        driver.switchTo().window(currentWindow);
+        Thread.sleep(3000);
+        driver.close();
+        driver.quit();
+    }
+}
